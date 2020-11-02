43b30905cf6e57982e842a85a3435524a528d298
Manny Singh <likebeats@gmail.com>
2020-11-01 19:21:51 -0500

    reset

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -3,8 +3,8 @@ export const maxPositionSize = 500;
 export const profitPercent = 0.15;
 export const stopLossPercent = 0.1;
 
-export const alpacaApiKey = 'PK97CC1QS8KY9XX01595';
-export const alpacaApiSecret = 'WwuxkcYJMKHZe9EZq2LIoO6x1p0vCxGA806nPrUP';
+export const alpacaApiKey = 'PKIXX85X7LVE6O735ZQ8';
+export const alpacaApiSecret = 'RncOWyfq9LsoKNoTUO6re24oQDlLyfrzekSYDFMX';
 
 export const finnhubSpreadPercent = 0.015;
 export const finnhubApiUrl = 'https://finnhub.io/api';
